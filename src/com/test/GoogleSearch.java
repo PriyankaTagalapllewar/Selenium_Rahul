f0c441d543dbace4db4cd70bae31b637d9969ad5
motarwar24@gmail.com <motarwar24@gmail.com>
2023-03-23 14:52:27 +0530

    first commit

---
+++ src/com/test/GoogleSearch.java
@@ -0,0 +1,10 @@
+package com.test;
+
+public class GoogleSearch {
+
+	public static void main(String[] args) {
+		
+      System.setProperty("webdriver.chrome.driver, "H:\Automation\chromedriver_win32\chromedriver.exe");
+	}
+
+}
